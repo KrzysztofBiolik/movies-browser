042ca92ad5dd8b7a3fb9c0fa728d6ca93a03668c
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-10-08 01:13:54 +0200

    test

--- src/common/Error/index.js
+++ src/common/Error/index.js
@@ -4,7 +4,7 @@ import { StyledError, StyledDanger, Warning, Info, Button, } from "./styled";
 export const Error = () => (
     <StyledError>
         <StyledDanger />
-        <Warning>Ooops! Something went wrong... </Warning>
+        <Warning>Ooops! Something went wrong.... </Warning>
         <Info>Please check your network connection <br/> and try again</Info>
         <Button to={toMovies()}>Back to home page</Button>
     </StyledError>
