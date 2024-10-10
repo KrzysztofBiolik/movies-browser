922b8a1c0a81d9fa4f291ccbc7978155aaec24ca
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-10-10 21:15:30 +0200

    Deleted movieSlice and movieSaga files and deleted it from rootSaga and store

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,10 +1,8 @@
 import { all } from "redux-saga/effects";
-import { movieSaga } from "../features/movies/MovieDetails/movieSaga";
 import { searchSaga } from "../common/Navigation/SearchBar/searchSaga";
 
 export default function* rootSaga() {
     yield all([
-        movieSaga(),
         searchSaga(),
     ]);
 };
--- src/core/store.js
+++ src/core/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
-import movieReducer from "../features/movies/MovieDetails/movieSlice";
 import searchReducer from "../common/Navigation/SearchBar/searchSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
-        movie: movieReducer,
         search: searchReducer, 
     },
 	middleware: (getDefaultMiddleware) =>
